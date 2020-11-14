c42974086c89ccddd74fc4a0be3c197bf158d0b2
hiroyuki-third-door <66162776+hiroyuki-third-door@users.noreply.github.com>
2020-11-14 14:49:07 +0900

    Create code.js

---
+++ code.js
@@ -0,0 +1,13 @@
+$('.slider').slick({
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 1500,
+
+});
+
+
+$(document).ready(function () {
+    $(".lightbox1").colorbox({
+        rel: 'lightbox1'
+    });
+});
